4e0631550ada9e85303b330bcda810450160a749
Tithilisa-Sen <sentithilisa@gmail.com>
2024-12-04 16:28:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Footer/>
     </div>
   );
 }
